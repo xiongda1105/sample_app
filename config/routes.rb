f97c804038b3fa4cc84ff081f72fcb21f9094056
vagrant <vagrant@localhost.localdomain>
2020-06-09 09:44:18 +0000

    Initial Commit Message

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'todolists/new'
+  get 'top' => 'homes#top'
+  post 'todolists' => 'todolists#create'
+
+
+  get 'todolists' => 'todolists#index'
+  get "todolists/:id" => "todolists#show", as: "todolist"
+end
